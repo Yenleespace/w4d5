a01db74ed484c5331d395f9502729fa2bc43c4cb
Yen Lee <yenny0104@gmail.com>
2022-12-02 14:00:02 -0500

    larget contiguous subsum

--- execution_time.rb
+++ execution_time.rb
@@ -12,7 +12,7 @@ def my_min(list)
     end  
  list.last
 end 
-list = [ 0, 3, 5, 4, -5, 10, 1, 90 ]
+# list = [ 0, 3, 5, 4, -5, 10, 1, 90 ]
 # p my_min(list)  # =>  -5
 
 #The time complexity is O(n^2) quadratic 
@@ -28,23 +28,23 @@ end
 
 # Largest Contiguous Sub-sum
 
-def largest_contiguous_subsum(list)
-    sub_arr = []
-    (0...list.length).each do |i|
-        (i...list.length).each do |j|
-            current_pair = list[i..j]
-            sub_arr << current_pair
-        end
-    end
-    largest = 0
-    new_arr = sub_arr.map do |sums|
-        sums.sum
-    end
-    new_arr.sort.last
-end
-
+# def largest_contiguous_subsum(list)
+#     sub_arr = []
+#     (0...list.length).each do |i|
+#         (i...list.length).each do |j|
+#             current_pair = list[i..j]
+#             sub_arr << current_pair
+#         end
+#     end
+#     largest = 0
+#     new_arr = sub_arr.map do |sums|
+#         sums.sum
+#     end
+#     new_arr.sort.last
+# end
+#The time complexity is O(n^2) and quadratic
 list = [5, 3, -7]
-   p  largest_contiguous_subsum(list) # => 8
+#    p  largest_contiguous_subsum(list) # => 8
 
     # possible sub-sums
     [5]           # => 5
@@ -53,3 +53,16 @@ list = [5, 3, -7]
     [3]           # => 3
     [3, -7]       # => -4
     [-7]          # => -7
+
+def largest_contiguous_subsum(list)
+    largest = list.first
+    largest_sum = 0
+    (1...list.length).each do |i|
+        if largest + list[i] > largest
+            largest_sum = largest + list[i]
+        end
+        # if list[i] > 
+    end
+    largest_sum
+end 
+p largest_contiguous_subsum(list)
