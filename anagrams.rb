de024cfb6fbdb466ecb60517712bb0836dfdf595
Yen Lee <yenny0104@gmail.com>
2022-12-02 14:53:23 -0500

    anagrams Phase 2

--- anagrams.rb
+++ anagrams.rb
@@ -22,5 +22,24 @@ def anagram?(string1, string2)
     # end 
 end 
 
-p anagram?("gizmo", "sally")    #=> false
-p anagram?("elvis", "lives")    #=> true
+# p anagram?("gizmo", "sally")    #=> false
+# p anagram?("elvis", "lives")    #=> true
+
+def second_anagram?(string1, string2)
+    new_str = ""
+    string1.each_char do |char|
+        if !string2.include?(char)
+            # string2.delete(char)
+            new_str += ""
+        else
+            new_str += char
+        end
+    end
+    # p string2
+    return false if new_str.empty?
+    return true
+end
+
+# p second_anagram?("gizmo", "sally")    #=> false
+# p second_anagram?("elvis", "lives")    #=> true
+
