2ed9c73033b2a17ab5d32047b4d2b3197448688c
Yen Lee <yenny0104@gmail.com>
2022-12-02 12:16:04 -0500

    largest_contiguous_sub_sum

--- execution_time.rb
+++ execution_time.rb
@@ -13,10 +13,40 @@ def my_min(list)
  list.last
 end 
 list = [ 0, 3, 5, 4, -5, 10, 1, 90 ]
-p my_min(list)  # =>  -5
+# p my_min(list)  # =>  -5
 
 #The time complexity is O(n^2) quadratic 
 
 def my_min(list)
+    smallest = list.first
+    (1...list.length - 1).each do |i|
+        smallest = list[i] if smallest > list[i]
+    end
+    smallest
 end 
 #The time complexity is O(n)
+
+# Largest Contiguous Sub-sum
+
+def largest_contiguous_subsum(list)
+    sub_arr = []
+    (0...list.length - 1).each do |i|
+        pairs = []
+        (i...list.length - 1).each do |j|
+            current_pair = list[i..j]
+        end
+        sub_arr << pairs
+    end
+    sub_arr
+end
+
+list = [5, 3, -7]
+   p  largest_contiguous_subsum(list) # => 8
+
+    # possible sub-sums
+    [5]           # => 5
+    [5, 3]        # => 8 --> we want this one
+    [5, 3, -7]    # => 1
+    [3]           # => 3
+    [3, -7]       # => -4
+    [-7]          # => -7
